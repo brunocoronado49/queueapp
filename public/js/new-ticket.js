ec994754223b21a2b38912e31d765014fc8a54e1
brunocoronado49 <francorangelcoronado@gmail.com>
2026-04-16 14:26:02 -0600

    ✨ Create new ticket in frontend

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,3 +1,19 @@
+const label = document.querySelector('span');
+const createTicketButton = document.querySelector('button');
 
+async function getLastTicket() {
+  const lastTicket = await fetch('/api/ticket/last').then(resp => resp.json());
+  label.innerText = lastTicket;
+}
 
-console.log('Nuevo Ticket HTML');
+async function createTicket() {
+  const newTicket = await fetch('api/ticket', {
+    method: 'POST',
+  }).then(resp => resp.json());
+
+  label.innerText = newTicket.number;
+}
+
+getLastTicket();
+
+createTicketButton.addEventListener('click', createTicket);
