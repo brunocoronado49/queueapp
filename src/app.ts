f991ddec85f9075a09839c8476a9aded9e0c23f5
brunocoronado49 <francorangelcoronado@gmail.com>
2026-04-16 11:27:46 -0600

    ✨ Update app for use the http node and Websocket server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+import { createServer } from 'http';
 import { envs } from './config/envs';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
+import { WssService } from './presentation/services/wss.service';
 
 (async () => {
   main();
@@ -13,5 +15,10 @@ async function main() {
     public_path: 'public',
   });
 
-  await server.start();
+  const httpServer = createServer(server.app);
+  WssService.initWss({ server: httpServer });
+
+  httpServer.listen(envs.PORT, () => {
+    console.log(`Server running on port: ${envs.PORT}`);
+  });
 }
